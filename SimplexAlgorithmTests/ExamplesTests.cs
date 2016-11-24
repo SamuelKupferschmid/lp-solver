f15ae7e4bc8cc18db32697c0e8ab59ac03afa5ac
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2016-11-24 18:36:06 +0100

    add tests

--- SimplexAlgorithmTests/ExamplesTests.cs
+++ SimplexAlgorithmTests/ExamplesTests.cs
@@ -13,5 +13,40 @@ namespace SimplexAlgorithmTests
             var s = new Solver("Examples\\BasicExample.csv");
             Assert.AreEqual(Solver.ResultType.OneResult, s.Solve());
         }
+
+        [TestMethod]
+        public void InfiniteSolutions()
+        {
+            var s = new Solver("Examples\\InfiniteSolutions.csv");
+            Assert.AreEqual(Solver.ResultType.InfinitResults, s.Solve());
+        }
+
+        [TestMethod]
+        public void NegSchlupf()
+        {
+            var s = new Solver("Examples\\NegSchlupf.csv");
+            Assert.AreEqual(Solver.ResultType.OneResult, s.Solve());
+        }
+
+        [TestMethod]
+        public void NichtNegativitaet_1()
+        {
+            var s = new Solver("Examples\\NichtNegativitaet_1.csv");
+            Assert.AreEqual(Solver.ResultType.OneResult, s.Solve());
+        }
+
+        [TestMethod]
+        public void NichtNegativitaet_2()
+        {
+            var s = new Solver("Examples\\NichtNegativitaet_2.csv");
+            Assert.AreEqual(Solver.ResultType.OneResult, s.Solve());
+        }
+
+        [TestMethod]
+        public void ZweiSaefte()
+        {
+            var s = new Solver("Examples\\ZweiSaefte.csv");
+            Assert.AreEqual(Solver.ResultType.OneResult, s.Solve());
+        }
     }
 }
---
+++ SimplexAlgorithmTests/SolverTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimplexAlgorithm;
+
+namespace SimplexAlgorithmTests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void FindInfiniteSolutions()
+        {
+            var cond = new Equation(Variable.Slack(1), new []
+            {
+                new VariableFactor(Variable.Problem(1),2),
+                new VariableFactor(Variable.Problem(2),1)  
+            },40);
+            var target = new Equation(Variable.Target(), new[]
+            {
+                new VariableFactor(Variable.Problem(1), 2),
+                new VariableFactor(Variable.Problem(2), 1)
+            }, 40);
+
+            var s = new Solver(new Equation[]{ cond, target });
+
+            Assert.AreEqual(Solver.ResultType.InfinitResults, s.Solve());
+        }
+    }
+}
